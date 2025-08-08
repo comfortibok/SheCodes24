028bb6c4fc8b327ac593fa580ab1603fbb92c462
comfortibok <ibokcomfort93@gmail.com>
2025-08-09 00:48:52 +0100

    Updated

---
+++ French_Poem_Generator (AI)/src/index.js
@@ -0,0 +1,25 @@
+function displayPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: "",
+    delay: 1.5,
+  });
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let apikey = "5of280a2b12980f017dcf8ff8fda334t";
+  let prompt = `User instructions: Generate a french poem about ${searchInput.value}`;
+  let context =
+    "You are an expert in writing poems. Generate a clear and short poem of 4 lines separating each line with <br/>. Do not include title. Sign the poem with 'Comfort Ibok' inside a strong element at the end NOT at the beginning";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}`;
+  let poem = document.querySelector("#poem");
+  poem.classList.remove("hidden");
+  poem.innerHTML = `<div class="generating">⏳ Generating a French Poem about ${searchInput.value}.</div>`;
+  axios.get(apiURL).then(displayPoem);
+}
+
+let poemElement = document.querySelector("form");
+poemElement.addEventListener("submit", handleSubmit);
